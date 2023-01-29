b50facc9837e12230a1121129b7bd469012b82fd
umair2607 <umair.k2607@gmail.com>
2023-01-29 23:46:27 +0500

    added details & home page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <Routes>
+        <Route path="/" element={<SearchParams />} />
+        <Route path="/details/:id" element={<Details />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
