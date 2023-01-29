0f25510a0e42b7d3c119dab4588e180271103c97
umair2607 <umair.k2607@gmail.com>
2023-01-30 01:19:21 +0500

    used react query to fetch and cacche pets details

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,29 @@ import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">Adopt Me!</Link>
-      </header>
-      <Routes>
-        <Route path="/" element={<SearchParams />} />
-        <Route path="/details/:id" element={<Details />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/" element={<SearchParams />} />
+          <Route path="/details/:id" element={<Details />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,8 +1,36 @@
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchPet";
 
 const Details = () => {
   const { id } = useParams();
-  return <h2>{id}</h2>;
+  const queryKey = ["details", id];
+  const results = useQuery(queryKey, () => fetchPet(queryKey));
+
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">🌀</h2>
+      </div>
+    );
+  }
+
+  if (results.isError) {
+    return <h1>Error</h1>;
+  }
+
+  const pet = results.data.pets[0];
+
+  return (
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
+        <button>Adopt {pet.name}</button>
+        <p>{pet.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Details;
