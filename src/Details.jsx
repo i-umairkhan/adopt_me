51f64dc1c4dec82a1171d78b1b9ebc0b9c99c734
umair2607 <umair.k2607@gmail.com>
2023-02-06 12:33:21 +0500

    added modal in details page using portals

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,10 +1,13 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import Modal from "./Modal";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carosuel";
 import ErrorBoundary from "./ErrorBoundry";
 
 const Details = () => {
+  const [showModal, setShowModal] = useState(false);
   const { id } = useParams();
   const queryKey = ["details", id];
   const results = useQuery(queryKey, () => fetchPet(queryKey));
@@ -29,8 +32,19 @@ const Details = () => {
         <Carousel images={pet.images} />
         <h1>{pet.name}</h1>
         <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
-        <button>Adopt {pet.name}</button>
+        <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>;
         <p>{pet.description}</p>
+        {showModal ? (
+          <Modal>
+            <div>
+              <h1>Would you like to adopt {pet.name}?</h1>
+              <div className="buttons">
+                <button>Yes</button>
+                <button onClick={() => setShowModal(false)}>No</button>
+              </div>
+            </div>
+          </Modal>
+        ) : null}
       </div>
     </div>
   );
